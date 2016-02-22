545bd34c3b3d383399af385ae398dcd306918b78
lily healey <lily@lilyhealey.co.uk>
2016-02-22 15:38:23 -0500

    add ability to change icon

--- views/clock.php
+++ views/clock.php
@@ -1,17 +1,30 @@
-<script type="text/javascript" src="<? echo $host; ?>static/js/global.js"></script>
+<?
+$sql = "select id from objects where url='settings'";
+$settings_id = $db->query($sql)->fetch_assoc()['id'];
+if($settings_id)
+{
+	$m = $oo->media($settings_id)[0];
+	if($m)
+		$img = m_url($m);
+	else
+		$img = "/media/svg/yorick.svg";
+}
+else
+	$img = "/media/svg/yorick.svg";
+?><script type="text/javascript" src="<? echo $host; ?>static/js/global.js"></script>
 <script type="text/javascript" src="<? echo $host; ?>static/js/clock.js"></script><?
 if(!$uu->id)
 {
 ?>
 <div id="skull-container" class="v-centre">
-	<img id="clock-canvas" class="large skull" src="/media/svg/yorick.svg">
+	<img id="clock-canvas" class="large skull" src="<? echo $img ?>">
 </div><?
 }
 else
 {
 ?>
 	<div id="skull-container" class="lower-right">
-		<img id="clock-canvas" class="small skull" src="/media/svg/yorick.svg">
+		<img id="clock-canvas" class="small skull" src="<? echo $img; ?>">
 	</div><?
 }
 ?><script>
